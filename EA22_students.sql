6d09973d386b3e0ab86a2f25201f3f351905fdb4
BrunoPalaciosc <118205482+BrunoPalaciosc@users.noreply.github.com>
2022-11-15 11:09:34 -0600

    Add files via upload
    Código para insertar un estudiante y todas las materias.

---
+++ EA22_students.sql
@@ -0,0 +1,36 @@
+INSERT INTO students (username, name, last_name, birthdate)
+VALUES ("17186", "Bruno", "Palacios", "2005/08/09");
+
+
+
+INSERT INTO courses (name, created_at)
+VALUES ("ARCM", "2022/11/15");
+
+INSERT INTO courses (name, created_at)
+VALUES ("AAD", "2022/11/15");
+
+INSERT INTO courses (name, created_at)
+VALUES ("TDAD", "2022/11/15");
+
+INSERT INTO courses (name, created_at)
+VALUES ("CS", "2022/11/15");
+
+INSERT INTO courses (name, created_at)
+VALUES ("CMEM", "2022/11/15");
+
+INSERT INTO courses (name, created_at)
+VALUES ("DSW", "2022/11/15");
+
+INSERT INTO courses (name, created_at)
+VALUES ("MA", "2022/11/15");
+
+INSERT INTO courses (name, created_at)
+VALUES ("MTDF", "2022/11/15");
+
+INSERT INTO courses (name, created_at)
+VALUES ("TOEFL", "2022/11/15");
+
+INSERT INTO courses (name, created_at)
+VALUES ("MCAI", "2022/11/15");
+
+
