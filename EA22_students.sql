e56fc0dade3981ad7ffaf247cbaa40ea957aa7b5
BrunoPalaciosc <118205482+BrunoPalaciosc@users.noreply.github.com>
2022-11-15 19:38:45 -0600

    Update EA22_students.sql
    Se insertaron los valores de las calificaciones en "students_courses".

--- EA22_students.sql
+++ EA22_students.sql
@@ -34,3 +34,44 @@ INSERT INTO courses (name, created_at)
 VALUES ("MCAI", "2022/11/15");
 
 
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='ARCM';
+
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='MCAI';
+
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='AAD';
+
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='DSW';
+
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='TDAD';
+
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='MA';
+	
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='TOEFL';
+
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='MTDF';
+
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='CMEM';
+
+INSERT INTO students_courses (student_id, course_id, grade)
+
+	SELECT '1', id, '9.5' FROM courses WHERE name='CS';
+
+
