c299ea42f3f1925a04fbf89e019bc8d67ce146f6
Jimena Olivares R <jimena.olivares.rivera28@gmail.com>
2022-11-17 06:22:37 -0600

    Changing db

--- studentModel.php
+++ studentModel.php
@@ -1,7 +1,5 @@
 <?php
 
-$db = mysqli_connect("sql9.freemysqlhosting.net", "sql9572075", "C3RgP3HGhx", "students");
-
 class student{
 
 private $username;
@@ -19,7 +17,7 @@ public function __construct($username, $name, $last_name, $birthdate)
  
   public function getAll(){
 
-  
+    $db = mysqli_connect("sql9.freemysqlhosting.net", "sql9572075", "C3RgP3HGhx", "sql9572075");  
     $sql = "SELECT * FROM  students";
 
 	mysqli_query($db, $sql);
@@ -27,6 +25,7 @@ public function __construct($username, $name, $last_name, $birthdate)
 
   public function getOne($id){
 
+	$db = mysqli_connect("sql9.freemysqlhosting.net", "sql9572075", "C3RgP3HGhx", "sql9572075");
   	$sql = "SELECT * FROM students WHERE id=$id";
   	mysqli_query($db, $sql);
 
@@ -34,6 +33,7 @@ public function __construct($username, $name, $last_name, $birthdate)
 
   public function store ($new_name, $new_username, $new_last_name, $new_birthdate) {
 
+	$db = mysqli_connect("sql9.freemysqlhosting.net", "sql9572075", "C3RgP3HGhx", "sql9572075");
   	$sql = "INSERT INTO students(username, name, last_name, birthdate) VALUES ('$new_username', '$new_name', '$new_last_name', '$new_birthdate')";
   	mysqli_query($db, $sql);
 
@@ -42,6 +42,7 @@ public function __construct($username, $name, $last_name, $birthdate)
 
   public function update($id, $up_name, $up_username, $up_last_name, $up_birthdate){
 
+    $db = mysqli_connect("sql9.freemysqlhosting.net", "sql9572075", "C3RgP3HGhx", "sql9572075");
     $sql = "UPDATE students SET username = '$up_username', name='$up_name', last_name='$up_last_name', birthdate='$up_birthdate' WHERE id=$id";
     mysqli_query($db, $sql);
   	
@@ -50,6 +51,7 @@ public function __construct($username, $name, $last_name, $birthdate)
 
   public function delete($id){
 
+    $db = mysqli_connect("sql9.freemysqlhosting.net", "sql9572075", "C3RgP3HGhx", "sql9572075");
     $sql = "DELETE FROM students WHERE id=$id";
     mysqli_query($db, $sql);
 
