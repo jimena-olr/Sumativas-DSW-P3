4e9f0f797b9d3917160d6de3d07940518e97439e
BrunoPalaciosc <118205482+BrunoPalaciosc@users.noreply.github.com>
2022-11-17 15:05:28 -0600

    Update studentsController.php
    Se terminó de programar la lógica del programa

--- studentsController.php
+++ studentsController.php
@@ -8,11 +8,30 @@ $num1 = $_POST['action'];
 
 $stud = new student('placeholder','placeholder', 'placeholder', 'placeholder');
 
+$id = $_POST['id'];
+$name = $_POST['name'];
+$username = $_POST['username'];
+$last_name = $_POST['last_name'];
+$birthdate = $_POST['birthdate'];
+
+
+
+
 if ($num1 == 1){
-	$stud ->store('placeholder','placeholder', 'placeholder', 'placeholder');
+	$stud ->store($username, $name, $last_name, $birthdate);
 }
 
+if ($num1 == 2){
+	$stud ->update($id, $username, $name, $last_name, $birthdate);
 }
+
+if ($num1 == 3){
+	$stud ->delete($id);
+}
+
+
+
+
 
 
 
